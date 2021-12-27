bac9aca5900888f9c81a1040cbdd10ab456ad9f0
mihocsaszilard <mihocsa48@gmail.com>
2021-12-27 11:05:46 +0100

    Creating API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutomapperProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DataModels = StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.DomainModels;
+using System;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutomapperProfiles: Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>()
+            .ReverseMap();
+
+            CreateMap<DataModels.Gender, Gender>()
+            .ReverseMap();
+
+            CreateMap<DataModels.Address, Address>()
+            .ReverseMap();
+        }
+    }
+}
